0bf5b71fb032380d417f9bbe712f266785074cb3
jasontung2214 <jasontung2214@gmail.com>
2020-05-05 17:19:06 +0800

    1. initialize

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
+public class GameController : MonoBehaviour
+{
+    public RandomColorController randomColorController;
+    public int beginTime = 20;
+    public int addTime = 1;
+
+    public GameObject startingUI;
+    public GameObject endingUI;
+    public Image wrongImage;
+    public TextMeshProUGUI levelText;
+    public TextMeshProUGUI timeText;
+    public LevelSetting[] levelSettings;
+    private int currentLevel;
+    private int CurrentLevel
+    {
+        set
+        {
+            currentLevel = value;
+            levelText.text = currentLevel.ToString();
+        }
+        get
+        {
+            return currentLevel;
+        }
+    }
+
+    private float currentTime;
+    private float CurrentTime
+    {
+        set
+        {
+            currentTime = value;
+            currentTime = Mathf.Max(0, currentTime);
+            timeText.text = Mathf.RoundToInt(currentTime).ToString();
+        }
+        get
+        {
+            return currentTime;
+        }
+    }
+
+    private IEnumerator Start()
+    {
+        yield return Starting();
+        yield return Playing();
+        yield return Ending();
+    }
+
+    public IEnumerator Starting()
+    {
+        currentTime = beginTime;
+        startingUI.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        startingUI.SetActive(false);
+    }
+
+    private IEnumerator Playing()
+    {
+        randomColorController.onAnswerClick += OnAnswerClick;
+        randomColorController.onWrongClick += OnWrongClick;
+        var levelSetting = GetLevelSetting();
+        randomColorController.Refresh(levelSetting.column, levelSetting.randomRange);
+        while (CurrentTime > 0)
+        {
+            yield return null;
+            CurrentTime -= Time.deltaTime;
+        }
+        randomColorController.onAnswerClick -= OnAnswerClick;
+        randomColorController.onWrongClick -= OnWrongClick;
+    }
+
+    private IEnumerator Ending()
+    {
+        endingUI.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(0);
+    }
+
+    private void OnAnswerClick()
+    {
+        CurrentLevel++;
+        CurrentTime += addTime;
+        var levelSetting = GetLevelSetting();
+        randomColorController.Refresh(levelSetting.column, levelSetting.randomRange);
+    }
+
+    private void OnWrongClick()
+    {
+        CurrentTime -= addTime;
+        var color = wrongImage.color;
+        color.a = 1;
+        wrongImage.color = color;
+        wrongImage.DOFade(0, 0.25f);
+    }
+
+    private LevelSetting GetLevelSetting()
+    {
+        int index = Mathf.Min(currentLevel, levelSettings.Length - 1);
+        return levelSettings[index];
+    }
+}
---
+++ Assets/Scripts/RandomColorController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using DG.Tweening;
+public class RandomColorController : MonoBehaviour
+{
+    private Image[] images;
+    private Button[] buttons;
+    public GridLayoutGroup gridLayoutGroup;
+    public int maxWidth = 600;
+    public int differentIndex;
+    private int cellCount;
+    public event Action onAnswerClick;
+    public event Action onWrongClick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        images = GetComponentsInChildren<Image>();
+        buttons = GetComponentsInChildren<Button>();
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            int index = i;
+            buttons[i].onClick.AddListener(() => OnClick(index));
+            buttons[i].gameObject.SetActive(false);
+        }
+    }
+
+    public void Refresh(int colume, float colorRange)
+    {
+        cellCount = colume * colume;
+        UpdateLayout(colume);
+        SetRandomColor(colorRange);
+    }
+
+    private void UpdateLayout(int column)
+    {
+        int eachCellWidth = maxWidth / column;
+        gridLayoutGroup.cellSize = Vector2.one * eachCellWidth;
+        DOTween.Kill(this);
+        for (int i = 0; i < images.Length; i++)
+        {
+            if (i < cellCount)
+            {
+                images[i].gameObject.SetActive(true);
+                images[i].transform.localScale = Vector3.zero;
+                images[i].transform.DOScale(Vector3.one, 0.3f).SetId(this);
+            }
+            else
+                images[i].gameObject.SetActive(false);
+        }
+    }
+
+    private void SetRandomColor(float colorRange)
+    {
+        float randomH = UnityEngine.Random.value;
+        float randomS = UnityEngine.Random.Range(0.6f + colorRange, 1f - colorRange);
+        float differentS = randomS + colorRange * UnityEngine.Random.value > 0.5 ? 1 : -1;
+        Color randomColor = Color.HSVToRGB(randomH, randomS, 1);
+        Color differentColor = Color.HSVToRGB(randomH, differentS, 1);
+        differentIndex = UnityEngine.Random.Range(0, cellCount);
+        for (int i = 0; i < images.Length; i++)
+        {
+            if (i == differentIndex)
+                images[i].color = differentColor;
+            else
+                images[i].color = randomColor;
+        }
+    }
+
+    public void OnClick(int index)
+    {
+        if (index == differentIndex)
+        {
+            onAnswerClick?.Invoke();
+        }
+        else
+        {
+            onWrongClick?.Invoke();
+        }
+    }
+}
